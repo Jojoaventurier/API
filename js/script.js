07bac3cad027aed2d240696ef2ff305117d93848
Geoffroy Maurutto <geoffroy.maurutto@gmail.com>
2024-07-22 16:32:28 +0200

    exercise seems tobe working

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ const inputCP = document.querySelector(".cp")
 // Sélectionne l'élément HTML avec la classe "ville"
 const selectVille = document.querySelector(".ville")
 
-// création de la carte
-// var map = L.map('map').setView([51.505, -0.09], 13);
-// var map = L.map('map').setView([10, -10.0], 13);
-// // ajout d'une couche pour avoir un visuel de la carte
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+
+
+// var map = L.map('map').setView(`${ville.centre.coordinates[0]}`, `${ville.centre.coordinates[1]}`, 13);
+var map = L.map('map')//.setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
 
 // Ajoute un écouteur d'évènement "input" (pendant la saisie) au champ de code postal
 inputCP.addEventListener("input", () => {
@@ -26,7 +29,8 @@ inputCP.addEventListener("input", () => {
         // Une fois que les données JSON sont disponibles
         .then((data) => {
             // Affiche les données dans la console (pour debug si besoin)
-            console.log(data)
+            console.log(data);
+            map.setView([data[0]['centre']['coordinates'][1], data[0]['centre']['coordinates'][0]], 13) 
              // Parcours chaque objet "ville" dans les données récupérées
             data.forEach((ville) => {
                 // Créé un nouvel élément d'option HTML
@@ -37,17 +41,10 @@ inputCP.addEventListener("input", () => {
                 option.innerHTML = `${ville.nom}`
                 // Ajoute l'option à la liste de sélection de ville
                 selectVille.appendChild(option)
+                let marker = new L.marker([`${ville.centre.coordinates[1]}`, `${ville.centre.coordinates[0]}`]).addTo(map);
             })
-             data.forEach((ville) => {
-                var map = L.map('map').setView(`${ville.centre.coordinates[0]}`, `${ville.centre.coordinates[1]}`, 13);
-                // ajout d'une couche pour avoir un visuel de la carte
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-                var marker = L.marker([`${ville.centre.coordinates[0]}`, `${ville.centre.coordinates[1]}`]).addTo(map)
-            })
-            
         })
-})
- // création de la carte
-            // var map = L.map('map').setView([51.505, -0.09], 13);
+    })
+
             
             
